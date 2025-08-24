66a00e234ab151c186b3c2f61a182455b8e10d5e
abel ashenafi <ashenafiabel66@gmail.com>
2025-08-24 22:44:21 +0300

    test3

--- test.py
+++ test.py
@@ -1,27 +1,8 @@
 import pickle
 
-# Path to your .pkl file
-pkl_file = "graph.pkl"
-
-# Load the .pkl file
-with open(pkl_file, "rb") as f:
+with open("new_graph.pkl", "rb") as f:
     data = pickle.load(f)
 
-# Check the type
-print("Type of loaded object:", type(data))
-
-# If it's a NetworkX graph
-if isinstance(data, dict):
-    # Sometimes pickled graphs are stored as dict with 'nodes' and 'edges'
-    print("Keys in the dictionary:", data.keys())
-    if 'nodes' in data:
-        print("Sample nodes:", list(data['nodes'])[:5])
-    if 'edges' in data:
-        print("Sample edges:", list(data['edges'])[:5])
-elif 'networkx' in str(type(data)).lower():
-    # Direct NetworkX graph
-    print("Nodes:", list(data.nodes())[:5])
-    print("Edges:", list(data.edges(data=True))[:5])
-else:
-    # Unknown format
-    print(data)
+# Save with protocol 4 and no NumPy objects
+with open("g_s.pkl", "wb") as f:
+    pickle.dump(data, f, protocol=4)
