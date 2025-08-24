1fce2027247953051e2de78845978f565fab997d
abel ashenafi <ashenafiabel66@gmail.com>
2025-08-24 18:55:09 +0300

    gene graph

--- test.py
+++ test.py
@@ -1,16 +1,27 @@
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
+import pickle
 
-import numpy
-import torch
-import networkx
-import deepsnap
-import matplotlib
-import seaborn
-import scipy
-import sklearn 
-import torch_geometric
-import test_tube
-import tqdm
+# Path to your .pkl file
+pkl_file = "graph.pkl"
 
-print("All imports successful!")
+# Load the .pkl file
+with open(pkl_file, "rb") as f:
+    data = pickle.load(f)
+
+# Check the type
+print("Type of loaded object:", type(data))
+
+# If it's a NetworkX graph
+if isinstance(data, dict):
+    # Sometimes pickled graphs are stored as dict with 'nodes' and 'edges'
+    print("Keys in the dictionary:", data.keys())
+    if 'nodes' in data:
+        print("Sample nodes:", list(data['nodes'])[:5])
+    if 'edges' in data:
+        print("Sample edges:", list(data['edges'])[:5])
+elif 'networkx' in str(type(data)).lower():
+    # Direct NetworkX graph
+    print("Nodes:", list(data.nodes())[:5])
+    print("Edges:", list(data.edges(data=True))[:5])
+else:
+    # Unknown format
+    print(data)
